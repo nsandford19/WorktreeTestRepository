a7adad42c60e18355c890effaf8a67bdff446ff5
Noah Sandford <noahs@millerslab.com>
2023-07-17 14:00:18 -0500

    main commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello World");
+Console.WriteLine("I am in the main branch with no worktree");
